473774f4d15b553e3e2ba6e1d86a010b2d68539d
calonsomend1 <calonsomend1@gmail.com>
2025-03-12 12:54:53 +0100

    Documentacion generada

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,29 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Calculadora",
+            "name": "Calculadora",
+            "summary": "Clase\u0020para\u0020operaciones\u0020matem\u00E1ticas\u0020b\u00E1sicas",
+            "url": "classes/Calculadora.html"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Asumar\u0028\u0029",
+            "name": "sumar",
+            "summary": "Suma\u0020dos\u0020n\u00FAmeros",
+            "url": "classes/Calculadora.html#method_sumar"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Afactorial\u0028\u0029",
+            "name": "factorial",
+            "summary": "Calcula\u0020el\u0020factorial\u0020de\u0020un\u0020n\u00FAmero",
+            "url": "classes/Calculadora.html#method_factorial"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003AcelsiusAFahrenheit\u0028\u0029",
+            "name": "celsiusAFahrenheit",
+            "summary": "Convierte\u0020temperatura\u0020de\u0020Celsius\u0020a\u0020Fahrenheit",
+            "url": "classes/Calculadora.html#method_celsiusAFahrenheit"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
